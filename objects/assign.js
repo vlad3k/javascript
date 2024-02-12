9b3b8db484d0485bed7e17c601ac7988404e16e6
vlad3k <vladislav.poezzhaev@gmail.com>
2024-02-13 01:51:13 +0700

    fix: assign immutable

--- objects/assign.js
+++ objects/assign.js
@@ -4,14 +4,15 @@ const obj2 = { c: 5, d: 6 };
 const obj1 = { b: 3, c: 4 };
 
 function customAssign(target, ...sources) {
+  const result = { ...target };
+
   for (let i = sources.length - 1; i >= 0; i--) {
-    console.log(sources[i]);
     for (let prop in sources[i]) {
-      target[prop] = sources[i][prop];
+      result[prop] = sources[i][prop];
     }
   }
 
-  return target;
+  return result;
 }
 
 console.log('mutated target obj: ', customAssign(obj, obj1, obj2, obj3));
