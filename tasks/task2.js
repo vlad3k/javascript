043afd93ef8b056cc0c23ccf1f4ccb29a9450d9a
vlad3k <vladislav.poezzhaev@gmail.com>
2024-02-22 20:39:44 +0700

    feat: task2 - merge array and copy by assign

--- tasks/task2.js
+++ tasks/task2.js
@@ -65,7 +65,8 @@ const defaultPerson = {
     }
   },
   eyes: "blue",
-  family: ["mom", "dad"]
+  family: ["mom", "dad"],
+  properties: ['house', ['audi', 'bmw'], ['washing machine', ['teddy bear', 'bat']]],
 };
 
 const me = {
@@ -75,24 +76,27 @@ const me = {
     c: "short"
   },
   hobbies: ["golf", "tennis"],
-  family: ["wife", "kids", "dog"]
+  family: ["wife", "kids", "dog"],
+  properties: ['laptop', ['lexus'], ['dishwasher', ['barby', 'bat']]],
 };
+
+console.log(deepMerge(defaultPerson, me));
 
 function isObject(variable) {
   return typeof variable === 'object' && variable !== null;
 }
 
 function deepMerge(target, source) {
-  const result = { ...target };
+  const result = Object.create(target);
 
   for (let prop in source) {
     const sourceValue = source[prop];
 
     if (Array.isArray(sourceValue) && Array.isArray(result[prop])) {
-      result[prop] = [ ...result[prop], ...sourceValue ];
+      result[prop] = mergeArray(result[prop], sourceValue);
     }
     else if (isObject(sourceValue) && !Array.isArray(sourceValue)) {
-      result[prop] = { ...deepMerge(result[prop], sourceValue) };
+      result[prop] = Object.assign(result[prop], sourceValue);
     }
     else {
       result[prop] = source[prop];
@@ -102,4 +106,32 @@ function deepMerge(target, source) {
   return result;
 }
 
-console.log(deepMerge(defaultPerson, me));
+const arr1 = ['house', ['audi', 'bmw'], ['washing machine', ['teddy bear', 'bat']]];
+const arr2 = ['laptop', ['lexus', 'tesla'], ['dishwasher', ['barby', 'bat']]];
+
+// console.log(mergeArray(arr1, arr2));
+
+function mergeArray(target, source) {
+  let result = [];
+  const maxLength = Math.max(target.length, source.length);
+
+  for (let i = 0; i < maxLength; i++) {
+    if (Array.isArray(target[i]) && Array.isArray(source[i])) {
+      result.push(mergeArray(target[i], source[i]));
+      continue;
+    }
+    if (target[i]) {
+      result.push(target[i]);
+    }
+    if (source[i] && !target.includes(source[i])) {
+      result.push(source[i]);
+    }
+  }
+
+  return result;
+}
+
+// ['house', ['audi', 'bmw'], ['washing machine', ['teddy bear', 'bat']]],
+// ['laptop', ['lexus'], ['dishwasher', ['barby', 'bat']]],
+
+// ['house', ['audi', 'bmw', 'lexus'], ['washing machine', 'dishwasher', ['teddy bear', 'barby', 'bat']]],
